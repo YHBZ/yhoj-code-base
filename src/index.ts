49669037a491189738e92c78620756f2d4aadef8
sjc0910 <sjc.0910@qq.com>
2022-02-03 16:02:40 +0800

    bugfix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express = require('express');
 import http = require('http');
 import path = require('path');
-import config = require('../config.json');
 import crypto = require('crypto');
+import fs = require('fs');
 var App = express();
 var server = http.createServer(App);
+
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'config.json')).toString());
 
 server.listen(config.port, config.host, () => {
     console.log("Start listening...");
